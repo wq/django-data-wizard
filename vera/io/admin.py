15003949816e8c318a114d049ded3a00475ba1b6
S. Andrew Sheppard <andrew@wq.io>
2013-06-14 16:35:11 -0500

    more robust to various table layouts

--- vera/io/admin.py
+++ vera/io/admin.py
@@ -1,4 +1,13 @@
-from .models import MetaColumn
 from wq.db.patterns import admin
+from wq.db.patterns.models import Relationship
+from .models import MetaColumn, Range
+
+class RangeInline(admin.TabularInline):
+    model = Range
+    extra = 0
+
+class RelationshipAdmin(admin.ModelAdmin):
+    inlines = [RangeInline]
 
 admin.site.register(MetaColumn, admin.IdentifiedModelAdmin)
+admin.site.register(Relationship, RelationshipAdmin)
--- vera/io/tasks.py
+++ vera/io/tasks.py
@@ -7,6 +7,7 @@ from wq.db.patterns.base import swapper
 from wq.db.contrib.files.models import File
 from .models import MetaColumn, UnknownItem, Range
 from django.conf import settings
+import datetime
     
 from wq.db.rest.models import get_ct, get_object_id
 Parameter = swapper.load_model('annotate', 'AnnotationType')
@@ -21,6 +22,11 @@ CONTENT_TYPES = {
     UnknownItem: get_ct(UnknownItem),
 }
 
+DATE_FIELDS = {
+    'DateTimeField': datetime.datetime,
+    'DateField':     datetime.date,
+}
+
 if not CONTENT_TYPES[Parameter].is_identified:
     raise Exception("AnnotationType should be swapped for an IdentifiedModel!"
                  +  "\n(HINT: set WQ_ANNOTATIONTYPE_MODEL='vera.Parameter')")
@@ -33,7 +39,8 @@ PRIORITY = {
 
 def load_file(file_obj):
     filename = "%s/%s" % (settings.MEDIA_ROOT, file_obj.file.name)
-    return load_file_io(filename)
+    options = load_file_options(file_obj)
+    return load_file_io(filename, options=options)
 
 def get_choices(file_obj):
     def make_list(cls, name):
@@ -62,89 +69,132 @@ def get_choices(file_obj):
     ]
 
 @task
-def read_columns(file, with_rels=False):
-    table = load_file(file)
-    matched_columns = []
-    if with_rels:
-        rels = []
-
-    existing = file.relationships.filter(
-        type__name='Contains Column'
-    ).order_by('range__start_column')
-
-    if existing.count() > 0:
-        if with_rels:
-            rels = existing
-        for rel in existing:
-            item = rel.right
-            rng = rel.range_set.all()[0]
-            if rng.start_row == table.header_row:
-                col = rng.start_column
-                info = {
-                    'name': table.field_map.keys()[col],
-                    'match': unicode(rel.right),
-                    'column': colname(col),
-                }
-                if isinstance(rel.right, UnknownItem):
-                    info['unknown'] = True
-                matched_columns.append(info)
-        
+def read_columns(file):
+    if already_parsed(file):
+        matched = load_columns(file)
     else:
-        for i, (name, attr_name) in enumerate(table.field_map.items()):
-            matches = list(Identifier.objects.filter_by_identifier(name))
-            if len(matches) > 0:
-                matches.sort(key=lambda ident: PRIORITY[ident.content_type.name])
-                column = matches[0].content_object
-                ctype = matches[0].content_type
-            else:
-                column = UnknownItem.objects.find(name)
-                ctype  = CONTENT_TYPES[UnknownItem]
+        matched = parse_columns(file)
 
-            reltype, is_new = RelationshipType.objects.get_or_create(
-                from_type    = CONTENT_TYPES[File],
-                to_type      = ctype,
-                name         = 'Contains Column',
-                inverse_name = 'Column In'
-            )
-            rel = file.relationships.create(
-                type              = reltype,
-                to_content_type   = ctype,
-                to_object_id      = column.pk,
-            )
-            Range.objects.create(
-                relationship = rel,
-                start_row = table.header_row,
-                start_column = i,
-                end_row = table.header_row,
-                end_column = i
-            )
-            info = {
-                'name': name,
-                'match': unicode(column),
-                'column': colname(i),
-            }
-            if isinstance(column, UnknownItem):
-                info['unknown'] = True
-            matched_columns.append(info)
-            if with_rels:
-                rels.append(rel)
-
-    for info in matched_columns:
+    for info in matched:
         if info.get('unknown', False):
             info['types'] = get_choices(file)
 
-    if with_rels:
-        return matched_columns, rels
-    else:
-        return matched_columns
+    return matched
+
+def already_parsed(file):
+    return file.relationships.filter(
+        type__name = 'Contains Column',
+        range__type = 'list'
+    ).exists()
+
+def load_file_options(file):
+    rels = file.relationships.filter(type__name = 'Contains Column')
+    headers = rels.filter(range__type='list').distinct()
+    if headers.count() == 0:
+        return {}
+    header_row = headers[0].range_set.get(type='head').start_row
+    start_row  = headers[0].range_set.get(type='list').start_row
+    return {
+        'header_row': header_row, 
+        'start_row': start_row
+    }
+
+def load_columns(file):
+    rels = file.relationships.filter(type__name = 'Contains Column')
+    table = load_file(file)
+    
+    matched = []
+    for rel in rels:
+        item = rel.right
+        info = {
+            'match': unicode(item),
+            'rel_id': rel.pk,
+        }
+        if isinstance(item, UnknownItem):
+            info['unknown'] = True
+
+        if rel.range_set.filter(type='list').exists():
+            col = rel.range_set.get(type='list').start_column
+            info['name'] = table.field_map.keys()[col].replace('\n', ' - ')
+            info['column'] = colname(col)
+            info['colnum'] = col
+
+        elif rel.range_set.filter(type='value').exists():
+            info['name'] = get_range_value(table, rel.range_set.get(type='head'))
+            info['value'] = get_range_value(table, rel.range_set.get(type='value'))
+        matched.append(info)
+    matched.sort(key=lambda info: info.get('colnum', -1))
+    return matched
+   
+def get_range_value(table, rng):
+    val = ""
+    for r in range(rng.start_row, rng.end_row + 1):
+        for c in range(rng.start_column, rng.end_column + 1):
+            val += table.extra_data.get(r, {}).get(c, "")
+    return val
+
+def parse_columns(file):
+    table = load_file(file)
+    matched = []
+    for i, (name, attr_name) in enumerate(table.field_map.items()):
+        matches = list(Identifier.objects.filter_by_identifier(name))
+        if len(matches) > 0:
+            matches.sort(key=lambda ident: PRIORITY.get(ident.content_type.name, 0))
+            column = matches[0].content_object
+            ctype = matches[0].content_type
+        else:
+            column = UnknownItem.objects.find(name)
+            ctype  = CONTENT_TYPES[UnknownItem]
+
+        reltype, is_new = RelationshipType.objects.get_or_create(
+            from_type    = CONTENT_TYPES[File],
+            to_type      = ctype,
+            name         = 'Contains Column',
+            inverse_name = 'Column In'
+        )
+        rel = file.relationships.create(
+            type              = reltype,
+            to_content_type   = ctype,
+            to_object_id      = column.pk,
+        )
+        Range.objects.create(
+            relationship = rel,
+            type = 'head',
+            start_row = table.header_row,
+            start_column = i,
+            end_row = table.start_row - 1,
+            end_column = i
+        )
+        Range.objects.create(
+            relationship = rel,
+            type = 'list',
+            start_row = table.start_row,
+            start_column = i,
+            end_row = table.start_row + len(table),
+            end_column = i
+        )
+
+        info = {
+            'name': name.replace('\n', " - "),
+            'match': unicode(column),
+            'colnum': i,
+            'column': colname(i),
+            'rel_id': rel.pk,
+        }
+        if isinstance(column, UnknownItem):
+            info['unknown'] = True
+
+        matched.append(info)
+
+    return matched
 
 @task
 def update_columns(file, post):
-    matched_columns, rels = read_columns(file, True)
-    for col, rel in zip(matched_columns, rels):
+    matched = read_columns(file)
+    for col in matched:
         if not col.get('unknown', False):
             continue
-        val = post.get('column_%s' % col['column'], None)
+        val = post.get('rel_%s' % col['rel_id'], None)
         if not val:
             continue
 
@@ -170,6 +220,7 @@ def update_columns(file, post):
             name         = 'Contains Column',
             inverse_name = 'Column In'
         )
+        rel = file.relationships.get(pk=col['rel_id'])
         rel.type = reltype
         rel.right = obj
         rel.save()
@@ -182,32 +233,53 @@ def reset(file):
 
 @task
 def import_data(file):
-    columns, rels = read_columns(file, True)
+    matched = read_columns(file)
     table = load_file(file)
+
+    file_globals = {
+        'event_key': {},
+        'report_meta': {},
+        'param_vals': {}
+    }
+
+    def save_value(col, val, obj):
+        item = file.relationships.get(pk=col['rel_id']).right
+        if isinstance(item, Parameter):
+            obj['param_vals'][get_object_id(item)] = val
+        elif isinstance(item, MetaColumn):
+            if not val:
+                return
+            if item.type == 'event' and item.name in EVENT_KEY:
+                fld = Event._meta.get_field_by_name(item.name)[0].get_internal_type()
+                if fld in DATE_FIELDS and not isinstance(val, DATE_FIELDS[fld]):
+                    from dateutil.parser import parse
+                    val = parse(val)
+                    if fld == 'DateField':
+                        val = val.date()
+                obj['event_key'][item.name] = val
+            elif item.type == 'report':
+                obj['report_meta'][item.name] = val
+
+    for col in matched:
+        if 'value' in col:
+            save_value(col, col['value'], file_globals)
+
     def add_record(row):
-        event_key = {}
-        report_meta = {}
-        param_vals  = {}
-        for i, rel in enumerate(rels):
-            rel = rels[i]
-            val = row[i]
-            col = rel.right
-            if isinstance(col, Parameter):
-                param_vals[get_object_id(col)] = val
-            elif isinstance(col, MetaColumn):
-                if not val:
-                    continue
-                if col.type == 'event' and col.name in EVENT_KEY:
-                    event_key[col.name] = val
-                elif col.type == 'report':
-                    report_meta[col.name] = val
-        if len(event_key.keys()) < len(EVENT_KEY):
+        record = {
+            key: file_globals[key].copy()
+            for key in file_globals
+        }
+        for col in matched:
+            if 'colnum' in col:
+                save_value(col, row[col['colnum']], record)
+
+        if len(record['event_key'].keys()) < len(EVENT_KEY):
             return
-        report_meta['user_id'] = 1
+        record['report_meta']['user_id'] = 1
         Report.objects.create_report(
-            EventKey(**event_key),
-            param_vals,
-            **report_meta
+            EventKey(**record['event_key']),
+            record['param_vals'],
+            **record['report_meta']
         )
 
     map(add_record, table)
