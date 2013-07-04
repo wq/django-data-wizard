eba0f43e54248d8cfe42e65666e7fe3447480886
S. Andrew Sheppard <andrew@wq.io>
2013-07-04 01:02:03 -0500

    create event on report save

--- vera/serializers.py
+++ vera/serializers.py
@@ -5,7 +5,9 @@ from wq.db.rest.serializers import ModelSerializer
 
 from .models import Result
 from wq.db.patterns.base import swapper
+from wq.db.patterns.base.models import extract_nested_key
 Event = swapper.load_model('vera', 'Event')
+Report = swapper.load_model('vera', 'Report')
 
 class ResultSerializer(AnnotationSerializer):
     value = serializers.Field()
@@ -21,5 +23,19 @@ class ResultSerializer(AnnotationSerializer):
 class EventSerializer(ModelSerializer):
     annotations = serializers.Field()
 
+class ReportSerializer(ModelSerializer):
+    def from_native(self, data, files):
+        data = data.dict()
+        event_key = extract_nested_key(data, Event)
+        if event_key:
+            event, is_new = Event.objects.get_or_create_by_natural_key(*event_key)
+            data['event'] = event.pk
+        if 'request' in self.context and not data.get('user', None):
+            user = self.context['request'].user
+            if user.is_authenticated():
+                data['user'] = user.pk
+        return super(ReportSerializer, self).from_native(data, files)
+
 app.router.register_serializer(Result, ResultSerializer)
 app.router.register_serializer(Event, EventSerializer)
+app.router.register_serializer(Report, ReportSerializer)
