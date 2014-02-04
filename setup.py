c1b3fc68f79b376335d058d73d90b3ba11b810c7
S. Andrew Sheppard <andrew@wq.io>
2014-02-04 09:09:15 -0600

    convert README to markdown

--- setup.py
+++ setup.py
@@ -6,13 +6,27 @@ Reference implementation of the ERAV model, an extention to EAV with support for
 """
 
 
-def long_description():
-    """Return long description from README.rst if it's present
-    because it doesn't get installed."""
+def parse_markdown_readme():
+    """
+    Convert README.md to RST via pandoc, and load into memory
+    (fallback to LONG_DESCRIPTION on failure)
+    """
+    # Attempt to run pandoc on markdown file
+    import subprocess
     try:
-        return open(join(dirname(__file__), 'README.rst')).read()
+        subprocess.call(
+            ['pandoc', '-t', 'rst', '-o', 'README.rst', 'README.md']
+        )
+    except OSError:
+        return LONG_DESCRIPTION
+
+    # Attempt to load output
+    try:
+        readme = open(join(dirname(__file__), 'README.rst'))
     except IOError:
         return LONG_DESCRIPTION
+    return readme.read()
+
 
 setup(
     name='vera',
@@ -22,7 +36,7 @@ setup(
     url='http://wq.io/vera',
     license='MIT',
     description='Implementation of the ERAV data integration model',
-    long_description=long_description(),
+    long_description=parse_markdown_readme(),
     install_requires=[
         'wq==0.5.0',
         'celery',
