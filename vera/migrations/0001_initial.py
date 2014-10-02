72179129a83fad65aff314fc98e31d8a3edd11c6
S. Andrew Sheppard <andrew@wq.io>
2014-10-02 03:13:07 +0000

    separate migration for result
    to reduce potential for circular dependencies with apps that override the other models

--- vera/migrations/0001_initial.py
+++ vera/migrations/0001_initial.py
@@ -10,11 +10,9 @@ class Migration(migrations.Migration):
     dependencies = [
         swapper.dependency('auth', 'User'),
         swapper.dependency('vera', 'Event'),
-        swapper.dependency('vera', 'Report'),
         swapper.dependency('vera', 'ReportStatus'),
         swapper.dependency('vera', 'Parameter'),
         swapper.dependency('vera', 'Site'),
-        swapper.dependency('vera', 'Result'),
     ]
 
     operations = [
@@ -28,23 +26,6 @@ class Migration(migrations.Migration):
                 'db_table': 'wq_event',
                 'ordering': ('-date',),
                 'swappable': swapper.swappable_setting('vera', 'Event'),
-                'abstract': False,
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='EventResult',
-            fields=[
-                ('id', models.PositiveIntegerField(serialize=False, primary_key=True)),
-                ('event_date', models.DateField()),
-                ('result_value_numeric', models.FloatField(null=True, blank=True)),
-                ('result_value_text', models.TextField(null=True, blank=True)),
-                ('result_empty', models.BooleanField(default=False)),
-                ('event', models.ForeignKey(to=swapper.get_model_name('vera', 'Event'))),
-            ],
-            options={
-                'db_table': 'wq_eventresult',
-                'swappable': swapper.swappable_setting('vera', 'EventResult'),
                 'abstract': False,
             },
             bases=(models.Model,),
@@ -97,24 +78,6 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Result',
-            fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('value_numeric', models.FloatField(null=True, blank=True)),
-                ('value_text', models.TextField(null=True, blank=True)),
-                ('empty', models.BooleanField(default=False, db_index=True)),
-                ('report', models.ForeignKey(to=swapper.get_model_name('vera', 'Report'), related_name='results')),
-                ('type', models.ForeignKey(to=swapper.get_model_name('vera', 'Parameter'))),
-            ],
-            options={
-                'ordering': ('type',),
-                'db_table': 'wq_result',
-                'swappable': swapper.swappable_setting('vera', 'Result'),
-                'abstract': False,
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
             name='Site',
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
@@ -132,10 +95,6 @@ class Migration(migrations.Migration):
             name='site',
             unique_together=set([('latitude', 'longitude')]),
         ),
-        migrations.AlterIndexTogether(
-            name='result',
-            index_together=set([('type', 'report', 'empty')]),
-        ),
         migrations.AlterUniqueTogether(
             name='reportstatus',
             unique_together=set([('slug',)]),
@@ -151,34 +110,6 @@ class Migration(migrations.Migration):
             name='user',
             field=models.ForeignKey(related_name='vera_report', to=swapper.get_model_name('auth', 'User'), null=True, blank=True),
             preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='eventresult',
-            name='event_site',
-            field=models.ForeignKey(to=swapper.get_model_name('vera', 'Site'), null=True, blank=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='eventresult',
-            name='result',
-            field=models.ForeignKey(to=swapper.get_model_name('vera', 'Result')),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='eventresult',
-            name='result_report',
-            field=models.ForeignKey(to=swapper.get_model_name('vera', 'Report')),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='eventresult',
-            name='result_type',
-            field=models.ForeignKey(to=swapper.get_model_name('vera', 'Parameter')),
-            preserve_default=True,
-        ),
-        migrations.AlterUniqueTogether(
-            name='eventresult',
-            unique_together=set([('event', 'result_type')]),
         ),
         migrations.AddField(
             model_name='event',
