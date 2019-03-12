e038f1a5898740248b5359fa3067416050a6a26d
S. Andrew Sheppard <andrew@wq.io>
2019-03-12 16:38:17 +0900

    don't need check for wq.db

--- data_wizard/apps.py
+++ data_wizard/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.core.exceptions import ImproperlyConfigured
 from .compat import reverse
+from django.conf import settings
 
 
 class WizardConfig(AppConfig):
@@ -10,6 +11,9 @@ class WizardConfig(AppConfig):
     def ready(self):
         self.module.autodiscover()
         self.module.init_backend()
+
+        if 'wq.db.rest' in settings.INSTALLED_APPS:
+            return
 
         # FIXME: Drop this check in 2.0
         try:
