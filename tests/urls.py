3ba1956e770d6d7bb6965459a96ecf9b766b428e
S. Andrew Sheppard <andrew@wq.io>
2019-03-12 16:20:32 +0900

    fix include

--- tests/urls.py
+++ tests/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 
-
 if settings.WITH_WQDB:
     from wq.db import rest
-    wizard_urls = [rest.router.urls]
+    wizard_urls = [url(r'^', include(rest.router.urls))]
 else:
     wizard_urls = [url(r'^datawizard/', include('data_wizard.urls'))]
 
