d54ac69a31199340bbca6d6720d1075143961e6b
S. Andrew Sheppard <andrew@wq.io>
2019-03-12 13:32:40 +0900

    specify upload_to

--- data_wizard/sources/models.py
+++ data_wizard/sources/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class FileSource(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
-    file = models.FileField()
+    file = models.FileField(upload_to='datawizard/')
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
