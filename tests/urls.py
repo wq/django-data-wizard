833d9a07e526962e9c7b93d3ff0fe4a74a5e9e62
S. Andrew Sheppard <andrew@wq.io>
2019-08-21 14:47:25 +0900

    update test configuration

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
+from django import VERSION as DJANGO_VERSION
 
 if settings.WITH_WQDB:
     from wq.db import rest
     wizard_urls = [url(r'^', include(rest.router.urls))]
+elif DJANGO_VERSION[0] < 2:
+    wizard_urls = [
+        url(r'^datawizard/', include('data_wizard.urls', 'data_wizard'))
+    ]
 else:
     wizard_urls = [url(r'^datawizard/', include('data_wizard.urls'))]
 
