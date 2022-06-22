84b821bfea1d66d6a867e481e32a48d11ffc03ca
S. Andrew Sheppard <andrew@wq.io>
2022-06-22 11:05:37 +0900

    set AutoField to avoid migrations

--- data_wizard/apps.py
+++ data_wizard/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class WizardConfig(AppConfig):
     name = "data_wizard"
     verbose_name = "Data Wizard"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         self.module.autodiscover()
